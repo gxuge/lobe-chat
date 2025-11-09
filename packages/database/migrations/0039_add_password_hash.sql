52e21bc9b32867c537f7636464afe3892d402761
guozhongxu <635764803@qq.com>
2025-11-09 16:47:27 +0800

    修复数据库问题

--- packages/database/migrations/0039_add_password_hash.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "password_hash" text;
-
--- Add comment to explain the column
-COMMENT ON COLUMN "users"."password_hash" IS 'Hashed password for credentials-based authentication';
-
